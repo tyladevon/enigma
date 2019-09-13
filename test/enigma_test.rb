990e2068d1700e8a3577764ddde44b382d2d6996
Tyla Devon <tyladevon@gmail.com>
2019-09-12 21:14:48 -0600

    Add require 'date' to Enigma Test file. And create test for encrypt method for Enigma class.

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -1,4 +1,5 @@
 require './test_helper'
+require 'date'
 require './lib/enigma'
 
 class EnigmaTest < Minitest::Test
@@ -9,5 +10,15 @@ class EnigmaTest < Minitest::Test
 
   def test_it_exists
     assert_instance_of Enigma, @enigma
+  end
+
+  def test_encrypt
+    expected = ({encryption: "keder ohulw",
+                  key: "02715",
+                  date: "040895"
+      })
+
+    assert_equal expected, @enigma.encrypt("hello world", "02715", "040895")
+
   end
 end
