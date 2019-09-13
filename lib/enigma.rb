d632a9acc68314d8a06f953e7218edb128308321
Tyla Devon <tyladevon@gmail.com>
2019-09-12 21:15:44 -0600

    Add basic structure of encrypt method to Enigma class

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,3 +1,12 @@
 class Enigma
 
-end 
+  def encrypt(message, key, date)
+    encrypt = {:enryption =
+                :key =
+                :date = }
+
+    message.each do ||
+    end 
+  end
+
+end
