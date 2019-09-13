1b59529e3e735cd22522ae003b43ae5e4e90cebd
Tyla Devon <tyladevon@gmail.com>
2019-09-13 14:06:04 -0600

    Add generate method to generator class.

--- lib/generator.rb
+++ lib/generator.rb
@@ -2,7 +2,13 @@ class Generator
 
   def initialize
     # @date = date
-    # @number = number
+    # @number = generate
+  end
+
+  def generate
+    random_number = rand(0..99999).to_s
+    random_number = random_number.rjust(5, "0")
+    random_number
   end
 
 end
