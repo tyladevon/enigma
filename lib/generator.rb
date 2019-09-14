4c9bf3c01c91650dba137f4dbd9a2d1dba1594d4
Tyla Devon <tyladevon@gmail.com>
2019-09-14 15:19:59 -0600

    Rename generate method to generate_random_number and refactor.

--- lib/generator.rb
+++ lib/generator.rb
@@ -1,14 +1,8 @@
-class Generator
+module Generator
 
-  def initialize
-    # @date = date
-    # @number = generate
-  end
-
-  def generate
+  def generate_random_number
     random_number = rand(0..99999).to_s
-    random_number = random_number.rjust(5, "0")
-    random_number
+    random_number.rjust(5, "0")
   end
 
 end
