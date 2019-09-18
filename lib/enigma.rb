37e37453645c9aac8da6e117a54a886c953b7a3c
Tyla Devon <tyladevon@gmail.com>
2019-09-18 07:12:11 -0600

    Refactor conditional statement and argument

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -5,16 +5,20 @@ require './lib/decrypt'
 
 class Enigma
 
-  def encrypt(message, key = "", date = Date.today.strftime('%d%m%y'))
-    if key.length != 5
+  def encrypt(message, key = Generator.generate_random_number, date = Date.today.strftime('%d%m%y'))
+    if key.length == 5
+      key = key
+    else
       date = key
       key = Generator.generate_random_number
     end
     Encrypt.encrypt(message, key, date)
   end
 
-  def decrypt(message, key, date)
-    if key.length != 5
+  def decrypt(message, key = Generator.generate_random_number, date = Date.today.strftime('%d%m%y'))
+    if key.length == 5
+      key = key
+    else
       date = key
       key = Generator.generate_random_number
     end
