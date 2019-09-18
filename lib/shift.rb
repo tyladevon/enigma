c45040b12ae87dadd0d70f7bb6b190db58d1f672
Tyla Devon <tyladevon@gmail.com>
2019-09-18 07:11:31 -0600

    Refactor shift class to include date

--- lib/shift.rb
+++ lib/shift.rb
@@ -2,13 +2,14 @@ require './lib/generator'
 
 class Shift
 
-  def self.shift_message(key,date)
+  def self.shift_message(key, date)
     key_hash = Hash.new(0)
     key_hash["A"] = key.slice(0,2).to_i
     key_hash["B"] = key.slice(1,2).to_i
     key_hash["C"] = key.slice(2,2).to_i
     key_hash["D"] = key.slice(3,2).to_i
 
+    date = Date.today.strftime('%d%m%y')
     date_hash = Hash.new(0)
     new_date = date.to_i * date.to_i
     date_hash["A"] = new_date.digits[3]
@@ -17,6 +18,5 @@ class Shift
     date_hash["D"] = new_date.digits[0]
 
     key_hash.merge(date_hash){|letter, random_num, date_num| random_num + date_num}
-    require 'pry';binding.pry
   end
 end
