cb58f3aa45ea71ae7045b19c9887358402f85bc0
Tyla Devon <tyladevon@gmail.com>
2019-09-17 22:34:47 -0600

    Refactor command line encrypt. Remove pry

--- command/encrypt.rb
+++ command/encrypt.rb
@@ -9,8 +9,7 @@ output = @enigma.encrypt(message)
 output_file_name = ARGV.last
 
 encrypted_message = output[:encryption]
-# require 'pry';binding.pry
 output_file = File.open(output_file_name, "w")
 output_file.write(encrypted_message)
 
-print "Created #{output_file_name} with the key #{output[:key]} and date #{output[:date]}"
+puts "Created #{output_file_name} with the key #{output[:key]} and date #{output[:date]}"
