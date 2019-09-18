5a0ec583ec2493550ce73885bfbaf3a6bc10c245
Tyla Devon <tyladevon@gmail.com>
2019-09-17 22:07:41 -0600

    Refactor shift_message method. Now class method. Delete duplication of calling Generator class.

--- lib/shift.rb
+++ lib/shift.rb
@@ -1,15 +1,14 @@
+require './lib/generator'
 
 class Shift
 
-  def shift_message
-    key = Generator.generate_random_number
+  def self.shift_message(key,date)
     key_hash = Hash.new(0)
     key_hash["A"] = key.slice(0,2).to_i
     key_hash["B"] = key.slice(1,2).to_i
     key_hash["C"] = key.slice(2,2).to_i
     key_hash["D"] = key.slice(3,2).to_i
 
-    date = Generator.generate_date
     date_hash = Hash.new(0)
     new_date = date.to_i * date.to_i
     date_hash["A"] = new_date.digits[3]
@@ -17,6 +16,7 @@ class Shift
     date_hash["C"] = new_date.digits[1]
     date_hash["D"] = new_date.digits[0]
 
-    shift_hash = key_hash.merge(date_hash){|letter, random_num, date_num| random_num + date_num}
+    key_hash.merge(date_hash){|letter, random_num, date_num| random_num + date_num}
+    require 'pry';binding.pry
   end
 end
