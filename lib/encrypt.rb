62ea226d8b56850cf417e0988752c15d15f5534f
Tyla Devon <tyladevon@gmail.com>
2019-09-17 22:19:52 -0600

    Refactor encrypt method. Assign key and date shift to shift method

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -1,33 +1,17 @@
 require './lib/generator'
+require './lib/shift'
 
 class Encrypt
 
-
   def self.encrypt(message, key, date)
-
-    key_hash = Hash.new(0)
-    key_hash["A"] = key.slice(0,2).to_i
-    key_hash["B"] = key.slice(1,2).to_i
-    key_hash["C"] = key.slice(2,2).to_i
-    key_hash["D"] = key.slice(3,2).to_i
-
-    date_hash = Hash.new(0)
-    new_date = date.to_i * date.to_i
-    date_hash["A"] = new_date.digits[3]
-    date_hash["B"] = new_date.digits[2]
-    date_hash["C"] = new_date.digits[1]
-    date_hash["D"] = new_date.digits[0]
-
-    shift_hash = key_hash.merge(date_hash){|letter, random_num, date_num| random_num + date_num}
-
+    shift_hash = Shift.shift_message(key, date)
     library = ("a".."z").to_a << " "
 
       encrypted_message = message.downcase.chars.map.with_index do |letter, index|
-        # require 'pry';binding.pry
         shift = library.find_index(letter) + shift_hash.values[index % shift_hash.length]
         library[shift % library.length]
-        # returns array of string character--ie letters
       end.join
+
       {encryption: encrypted_message,
        key:        key,
        date:       date
