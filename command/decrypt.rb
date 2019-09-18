8cb3a2340be47dedb35e3dfdb78cf87946db4d5d
Tyla Devon <tyladevon@gmail.com>
2019-09-17 22:33:55 -0600

    Create command line for decrypt.rb

---
+++ command/decrypt.rb
@@ -0,0 +1,20 @@
+require './lib/enigma'
+
+
+input_file_name = ARGV.first
+input_file = File.open(input_file_name, "r")
+
+@enigma = Enigma.new
+
+message = input_file.read.chomp
+key = ARGV[2]
+date = ARGV[3]
+output = @enigma.decrypt(message, key, date)
+output_file_name = ARGV[1]
+
+decrypted_message = output[:decryption]
+output_file = File.open(output_file_name, "w")
+output_file.write(decrypted_message)
+
+
+puts "Created #{output_file_name} with the key #{output[:key]} and date #{output[:date]}"
